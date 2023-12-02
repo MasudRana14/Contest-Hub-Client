da5c13f8b512871106af90bf1303d50697c07134
MasudRana14 <iammasud34@gmail.com>
2023-12-02 21:41:33 +0600

    add signOut Page

--- src/LayOut/DashBoard.jsx
+++ src/LayOut/DashBoard.jsx
@@ -5,72 +5,84 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const DashBoard = () => {
 
+    const isAdmin = true;
+
     return (
         <div>
 
-           
+
             <div className="flex">
-                
-            {/* Dashboard Side Menu Bar  */}
-            <div className=" w-32 lg:w-64 min-h-screen bg-orange-400 ">
-            
-            {/* Title  */}
-            <div>
-                <h1 className="lg:text-xl text-center mt-3  font-bold text-blue-600">Contest Hub Platform</h1>
-                <div className="divider"></div>
+
+                {/* Dashboard Side Menu Bar  */}
+                <div className=" w-32 lg:w-64 min-h-screen bg-orange-400 ">
+
+                    {/* Title  */}
+                    <div>
+                        <h1 className="lg:text-xl text-center mt-3  font-bold text-blue-600">Contest Hub Platform</h1>
+                        <div className="divider"></div>
+                    </div>
+
+                    <ul className="menu lg:p-4">
+
+
+                        {
+                            isAdmin ? <>
+                                <li className="lg:font-bold lg:text-base">
+                                    <NavLink to="/dashboard/manageUser">Manage User</NavLink>
+                                </li>
+
+                                <li className="lg:font-bold lg:text-base">
+                                    <NavLink to="/dashboard/manageContest">Manage Contest</NavLink>
+                                </li>
+                                <li className="lg:font-bold lg:text-base ">
+                                    <NavLink to="/dashboard/addcontest">Add Contest</NavLink>
+                                </li>
+                            </>
+                                :
+                                <>
+
+                                    <li className="lg:font-bold lg:text-base ">
+
+                                        <NavLink to="/dashboard/profile"><FaUser></FaUser>My Profile</NavLink>
+                                    </li>
+
+
+                                    <li className="lg:font-bold lg:text-base">
+                                        <NavLink to="/dashboard/createdcontest">My Created Contest</NavLink>
+                                    </li>
+
+                                    <li className="lg:font-bold lg:text-base">
+                                        <NavLink to="/dashboard/submitted">Contest Submitted</NavLink>
+                                    </li>
+
+                                    {/* Divider  */}
+                                    <div className="divider">OR</div>
+
+                                    <li className="lg:font-bold lg:text-base">
+                                        <NavLink to="/dashboard/participate">My Participated Contest</NavLink>
+                                    </li>
+
+                                    <li className="lg:font-bold lg:text-base">
+                                        <NavLink to="/dashboard/winning">My Winning Contest</NavLink>
+                                    </li>
+
+                                    <li className="lg:font-bold lg:text-base">
+                                        <NavLink to="/">Home</NavLink>
+                                    </li>
+                                </>
+                        }
+
+
+
+                    </ul>
+                </div>
+                {/* Dashboard content  */}
+
+                <div className="flex-1">
+                    <Outlet></Outlet>
+                </div>
+
             </div>
-
-                <ul className="menu lg:p-4">
-
-                    <li className="lg:font-bold lg:text-base ">
-                       
-                        <NavLink to="/dashboard/profile"><FaUser></FaUser>My Profile</NavLink>
-                    </li>
-
-                    <li className="lg:font-bold lg:text-base">
-                        <NavLink to="/dashboard/manageUser">Manage User</NavLink>
-                    </li>
-
-                    <li className="lg:font-bold lg:text-base">
-                        <NavLink to="/dashboard/manageContest">Manage Contest</NavLink>
-                    </li>
-
-                    <li className="lg:font-bold lg:text-base ">
-                        <NavLink to="/dashboard/addcontest">Add Contest</NavLink>
-                    </li>
-
-                    <li className="lg:font-bold lg:text-base">
-                        <NavLink to="/dashboard/createdcontest">My Created Contest</NavLink>
-                    </li>
-
-                    <li className="lg:font-bold lg:text-base">
-                        <NavLink to="/dashboard/submitted">Contest Submitted</NavLink>
-                    </li>
-
-                        {/* Divider  */}
-                    <div className="divider">OR</div>
-
-                    <li className="lg:font-bold lg:text-base">
-                        <NavLink to="/dashboard/participate">My Participated Contest</NavLink>
-                    </li>
-
-                    <li className="lg:font-bold lg:text-base">
-                        <NavLink to="/dashboard/winning">My Winning Contest</NavLink>
-                    </li>
-
-                    <li className="lg:font-bold lg:text-base">
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-
-                </ul>
-            </div>
-            {/* Dashboard content  */}
-
-            <div className="flex-1">
-                <Outlet></Outlet>
-            </div>
-
-        </div>
         </div>
     );
 };
--- src/Pages/ManageUsers/ManageUser.jsx
+++ src/Pages/ManageUsers/ManageUser.jsx
@@ -1,13 +1,67 @@
-
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import { FaTrash, FaUser, FaUsers } from "react-icons/fa";
 
 const ManageUser = () => {
 
+    const axiosSecure = useAxiosSecure();
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/users');
+            return res.data;
+        }
+    })
+
+    const handleUserDelete = user =>{
+
+    } 
+
     return (
-        <div className="mt-7">
+        <div className="mt-7 w-3/4 mx-auto">
             <div className="flex justify-evenly">
                 <h3 className="text-2xl">All Users</h3>
-                <h3 className="text-2xl">Total Users</h3>
+                <h3 className="text-2xl">Total Users {users.length}</h3>
             </div>
+
+            {/* Table  */}
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td className="font-bold">{user.name}</td>
+                                <td className="font-medium">{user.email}</td>
+                                <td>
+                                    <button>
+                                        <FaUsers className="text-2xl text-blue-500"></FaUsers>
+                                    </button>
+                                </td>
+                                <button></button>
+                                <td>
+                                    <button>
+                                        <FaTrash className="text-lg text-red-500"></FaTrash>
+                                    </button>
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
